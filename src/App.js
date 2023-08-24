4bb356a13bbe3657f98dcd6fb4b28a012400ca72
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2023-08-24 16:30:14 -0300

    Estruturando projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
+import Home from './pages/Home';
+import Company from './pages/Company';
+import Contact from './pages/Contact';
+import NewProject from './pages/NewProject';
+
+import Container from './layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
-     
+
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/company'>Empresa</Link>
+        </li>
+        <li>
+          <Link to='/contact'>Contato</Link>
+        </li>
+        <li>
+          <Link to='/newproject'>Novo Projeto</Link>
+        </li>
+      </ul>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>footer</p>
+    </BrowserRouter>
+
   );
 }
 
