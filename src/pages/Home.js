e66345cffec3debf5cd6915807efbd79423589f8
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2023-08-25 16:40:00 -0300

    Criando a Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
+
+import savings from '../img/savings.svg';
+import LinkButton from '../layout/LinkButton';
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <section className='home-container'>
+      <h1>Bem-vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo</p>
+      <LinkButton to='/newproject' text='Criar Projeto' />
+      <img src={savings} alt='Costs' />
+    </section>
   )
 }
 
