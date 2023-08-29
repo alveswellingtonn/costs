ba9e585cf50f26811eb2b24dae4c93948387830e
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2023-08-29 17:13:28 -0300

    Criando Dashboard

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import './Projects.css'
+
 import Message from '../layout/Message'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton';
 
 import { useLocation } from 'react-router-dom'
 
@@ -8,17 +12,23 @@ function Projects() {
   const location = useLocation();
   let message = '';
 
-  if(location.state) {
+  if (location.state) {
     message = location.state.message;
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className='project-container'>
+      <div className='title-container'>
+        <h1>Meus Projetos</h1>
+        <LinkButton to='/newproject' text='Criar Projeto' />
+      </div>
       {message && <Message type="success" msg={message} />}
-      
+      <Container customClass="start" >
+        <p> Projetos...</p>
+      </Container>
+
     </div>
-    
+
   )
 }
 
