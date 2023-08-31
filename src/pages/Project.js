cee65ffa9cc2567c31f0132f3818c5bfc964cf9b
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2023-08-31 11:09:46 -0300

    Atualização de projeto

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './Project.css'
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import Message from '../layout/Message'
 import ProjectForm from '../project/ProjectForm'
 
 import { useParams } from 'react-router-dom'
@@ -11,6 +12,8 @@ function Project() {
     const { id } = useParams();
     const [project, setProject] = useState([]);
     const [showProjectForm, setShowProjectForm] = useState(false);
+    const [message, setMessage] = useState();
+    const [type, setType] = useState();
 
     useEffect(() => {
         setTimeout(() => {
@@ -30,13 +33,15 @@ function Project() {
     function editPost(project) {
         // budget validation
         if(project.budget < project.cost) {
-            //mensagem
+            setMessage('O orçamento não pode ser menor que o custo do projeto!')
+            setType('error')
+            return false
         }
 
         fetch(`http://localhost:5000/projects/${project.id}`, {
             method: 'PATCH',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json', 
             },
             body: JSON.stringify(project),
         })
@@ -44,7 +49,8 @@ function Project() {
         .then((data) => {
             setProject(data)
             setShowProjectForm(false)
-            //mensagem
+            setMessage('Projeto atualizado!')
+            setType('success')
         })
         .catch(err => console.log(err))
     }
@@ -58,6 +64,7 @@ function Project() {
             {project.name ? (
                 <div className='project-details'>
                     <Container customClass='column'>
+                        {message && <Message type={type} msg={message} />}
                         <div className='details-container'>
                             <h1>Projeto: {project.name}</h1>
                             <button className='btn' onClick={toggleProjectForm}>
